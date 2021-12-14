bf7f591a40b2d86eb1e016401bdd5cc9e808df6b
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-12-14 18:04:40 +0530

    Added: error details class

--- address-book-app/src/main/java/com/bridgelabz/addressbookapp/dto/ErrorDetailDto.java
+++ address-book-app/src/main/java/com/bridgelabz/addressbookapp/dto/ErrorDetailDto.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.addressbookapp.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
 public class ErrorDetailDto {
+    private Date timeStamp;
+    private String message;
+    private String details;
+
+    public ErrorDetailDto(Date timeStamp, String message, String details) {
+        this.timeStamp = timeStamp;
+        this.message = message;
+        this.details = details;
+    }
 }
